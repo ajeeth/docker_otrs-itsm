7d0ea528055c64ffca05d4abae283f8786ff87d5
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2018-06-19 00:27:46 +0400

    update to veriosn 6.0.8

--- firstrun.sh
+++ firstrun.sh
@@ -13,16 +13,13 @@ wait
 #/opt/otrs/bin/otrs.RebuildConfig.pl &
 su -c "/opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild" -s /bin/bash otrs &
 wait
-#/opt/otrs/bin/otrs.PackageManager.pl -a install -p /ITSM-5.0.8.opm &
-su -c "/opt/otrs/bin/otrs.Console.pl Admin::Package::Install /ITSM-5.0.16.opm" -s /bin/bash otrs &
+#/opt/otrs/bin/otrs.PackageManager.pl -a install -p /ITSM-6.0.8.opm &
+su -c "/opt/otrs/bin/otrs.Console.pl Admin::Package::Install /ITSM-6.0.8.opm" -s /bin/bash otrs &
 wait
 /opt/otrs/bin/Cron.sh start otrs &
 wait
-curl -o /tmp/Znuny4OTRS-Repo.opm http://portal.znuny.com/api/addon_repos/public/615/latest
+curl -o /tmp/Znuny4OTRS-Repo.opm https://addons.znuny.com/api/addon_repos/public/1029/latest
 #/opt/otrs/bin/otrs.PackageManager.pl -a install -p /tmp/Znuny4OTRS-Repo.opm &
 su -c "/opt/otrs/bin/otrs.Console.pl Admin::Package::Install /tmp/Znuny4OTRS-Repo.opm" -s /bin/bash otrs &
-wait
-curl -o /tmp/ServiceImportExport-5.0.0.opm http://opar.perl-services.de/package/download/1078
-su -c "/opt/otrs/bin/otrs.Console.pl Admin::Package::Install /tmp/ServiceImportExport-5.0.0.opm" -s /bin/bash otrs &
 wait
 rm /firstrun
